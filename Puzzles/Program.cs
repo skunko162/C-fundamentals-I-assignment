430ea2f9fadab6d24c6f7d6db007ba1a4c76b2e7
skunko162 <stvnkelly79@gmail.com>
2022-12-02 11:38:27 -0600

    Puzzles Practice

---
+++ Puzzles/Program.cs
@@ -0,0 +1,41 @@
+﻿    class Program
+    {
+    static void Main(string[] args)
+    {
+        string[] coin = { "heads", "tails" };
+        int user;
+        int heads = 0;
+        int tails = 0;
+        Random rand = new Random();
+        const int faces = 2;
+
+
+        Console.Write("enter 1 to flip a coin, or 2 to leave: ");
+        user = Convert.ToInt32(Console.ReadLine());
+
+        while (user == 1) 
+        {
+            int second = rand.Next(faces);
+            if (second == 1)
+            {
+                Console.WriteLine("coin landed on heads");
+                heads += 1;
+                Console.WriteLine($"you have landed on heads: {heads} time(s)");
+                
+                Console.WriteLine($"you have landed on tails: {tails} time(s)");
+            }
+            else
+            {
+                Console.WriteLine("coin landed on tails");
+                tails += 1;
+                Console.WriteLine($"you have landed on heads: {heads} time(s)");
+
+                Console.WriteLine($"you have landed on tails: {tails} time(s)");
+            }
+            Console.Write("enter 1 to flip again, or 2 to leave: ");
+            user = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.WriteLine("goodbye");
+    }
+    }
